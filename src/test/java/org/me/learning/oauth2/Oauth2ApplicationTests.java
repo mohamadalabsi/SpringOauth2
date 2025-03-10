b438ac5d88810673769b0d551803a8b8cfaf34a8
mohamadalabsi <absimoda1@gmail.com>
2025-03-10 23:36:49 +0100

    starting with spring Oauth2

---
+++ src/test/java/org/me/learning/oauth2/Oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.me.learning.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
